13841f8f401d4fb3f04fa5bc09d6f4ed443ea20c
Łukasz <ljedrasik@vuestorefront.io>
2021-03-31 13:03:22 +0200

    chore: update old branch

--- packages/core/nuxt-theme-module/theme/components/AppHeader.vue
+++ packages/core/nuxt-theme-module/theme/components/AppHeader.vue
@@ -125,8 +125,9 @@ export default {
   },
   directives: { clickOutside },
   setup(props, { root }) {
-    const { toggleCartSidebar, toggleWishlistSidebar, toggleLoginModal } = useUiState();
+    const { toggleCartSidebar, toggleWishlistSidebar, toggleAuthModal } = useUiState();
     const { setTermForUrl, getFacetsFromURL } = useUiHelpers();
+    const { result, search } = useFacet();
     const { isAuthenticated, load: loadUser } = useUser();
     const { cart, load: loadCart } = useCart();
     const { load: loadWishlist } = useWishlist();
@@ -148,7 +149,7 @@ export default {
         return root.$router.push('/my-account');
       }
 
-      toggleLoginModal();
+      toggleAuthModal();
     };
 
     onSSR(async () => {
